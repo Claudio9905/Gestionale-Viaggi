2a9b0257ee336c778b4113310167d18e0de4ca67
Claudio9905 <cloudpost10@gmail.com>
2025-10-17 10:49:26 +0200

    Update file

--- gestionaleviaggi/src/main/java/claudiopostiglione/gestionaleviaggi/configClass/CloudinaryConfig.java
+++ gestionaleviaggi/src/main/java/claudiopostiglione/gestionaleviaggi/configClass/CloudinaryConfig.java
@@ -1,7 +1,27 @@
 package claudiopostiglione.gestionaleviaggi.configClass;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary getImageUpLoader(@Value("${cloudinary.name}") String cloudName, @Value("${cloudinary.key}") String apiKey, @Value("${cloudinary.secret}") String apiSecret){
+
+        System.out.println("Cloudname: " + cloudName);
+        System.out.println("ApiKey: " + apiKey );
+        System.out.println("ApiSecret: " + apiSecret);
+
+        Map<String,String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
+    }
 }
