af713b0243ccc9c8d9d6d1b8ee88c4e340b4e5bf
Claudio9905 <cloudpost10@gmail.com>
2025-10-17 09:12:37 +0200

    Gestionale-Viaggi

---
+++ gestionaleviaggi/src/main/java/claudiopostiglione/gestionaleviaggi/GestionaleviaggiApplication.java
@@ -0,0 +1,13 @@
+package claudiopostiglione.gestionaleviaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleviaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleviaggiApplication.class, args);
+	}
+
+}
---
+++ gestionaleviaggi/src/test/java/claudiopostiglione/gestionaleviaggi/GestionaleviaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package claudiopostiglione.gestionaleviaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleviaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
